fc5178d65a281a672aee187e7aa5595957f4b7d6
Tahira Fulazzaky <85498210+fulazz@users.noreply.github.com>
2023-11-18 14:30:53 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-import seaborn as sns
 import plotly.express as px
 
 # Load data from GitHub
@@ -12,8 +11,13 @@ st.title("E-Commerce Public Dataset Dashboard")
 
 # Visualize product category distribution
 st.write("### Product Category Distribution:")
-fig_category = px.bar(df['product_category_name'].value_counts(), x='product_category_name', y='product_category_name',
-                      labels={'product_category_name': 'Count'}, title='Product Category Distribution')
+
+# Use plotly express to create a bar chart
+fig_category = px.bar(df['product_category_name'].value_counts().reset_index(), x='index', y='product_category_name',
+                      labels={'index': 'Product Category', 'product_category_name': 'Count'},
+                      title='Product Category Distribution')
+
+# Display the plot using st.plotly_chart
 st.plotly_chart(fig_category)
 
 # Display the raw data (optional)
@@ -24,25 +28,4 @@ if st.checkbox("Show Raw Data"):
 # Convert 'order_estimated_delivery_date' to datetime
 df['order_estimated_delivery_date'] = pd.to_datetime(df['order_estimated_delivery_date'])
 
-# Bar chart comparing seller_state and customer_state
-st.title('Olist E-commerce Dashboard: Seller State vs Customer State')
-fig_seller_customer_state = px.bar(df, x=['seller_state', 'customer_state'], title='Seller State vs Customer State')
-st.plotly_chart(fig_seller_customer_state)
-
-# Line graph showing trend order purchase by month
-df['month_year'] = pd.to_datetime(df['month_year'])
-st.title('Olist E-commerce Dashboard: Order Purchase Trend by Month')
-fig_order_trend = px.line(df.groupby('month_year').size().reset_index(name='order_count'), x='month_year', y='order_count', title='Order Purchase Trend by Month')
-st.plotly_chart(fig_order_trend)
-
-# Bar chart showing top 5 product sells
-st.title('Olist E-commerce Dashboard: Top 5 Product Sells')
-top_products = df['product_category_name'].value_counts().head(5)
-fig_top_products = px.bar(top_products, x=top_products.index, y=top_products.values, title='Top 5 Product Sells')
-st.plotly_chart(fig_top_products)
-
-# Monthly income from variable price and order_estimated_delivery_date
-st.title('Olist E-commerce Dashboard: Monthly Income')
-monthly_income = df.groupby('month_year')['price'].sum().reset_index()
-fig_monthly_income = px.bar(monthly_income, x='month_year', y='price', title='Monthly Income')
-st.plotly_chart(fig_monthly_income)
+# The rest of your Streamlit app remains the same...
